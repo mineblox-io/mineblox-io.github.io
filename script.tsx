cbf0bc719996ba434de8e8742b5058934a675b75
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-02-12 06:42:45 +0000

    Updated Buttons

--- script.tsx
+++ script.tsx
@@ -1,4 +1,15 @@
 const audio = new Audio("STAY.mp3");
 audio.loop = true;
 audio.volume = 1.0;
-audio.play;
+audio.play();
+const Play = document.getElementById("Play");
+Play?.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+function render() {
+    return <button type="button" id="Play">Play Audio</button>
+}
