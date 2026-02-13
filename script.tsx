812dee55b8a94d650b545f4433625b3a0e7cad3f
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-02-13 05:00:58 +0000

    Updated Cube Model

--- script.tsx
+++ script.tsx
@@ -12,8 +12,13 @@ audioLoader.load( 'Craig_Ballie_-_Building_Us_(mp3.pm).mp3', function( buffer ) 
 	sound.setVolume( 0.5 );
 	sound.play();
 });
+const loader = new THREE.CubeTextureLoader();
+loader.setPath( 'assets/textures/grass/' );
+const textureCube = loader.load( [
+	'px.svg', 'nx.svg', 'py.svg', 'ny.svg', 'pz.svg', 'nz.svg'
+] );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, envMap: textureCube} );
 const cube = new THREE.Mesh( geometry, material );
 const scene = new THREE.Scene();
 scene.add(cube);
