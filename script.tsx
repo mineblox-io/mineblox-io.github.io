e5e670b7b22c06cd5fff911ed720aba51066528e
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-02-13 05:03:44 +0000

    Deleted All Comments

--- script.tsx
+++ script.tsx
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import * as react from 'react';
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-// create an AudioListener and add it to the camera
 const listener = new THREE.AudioListener();
 camera.add( listener );
 const sound = new THREE.Audio( listener );
