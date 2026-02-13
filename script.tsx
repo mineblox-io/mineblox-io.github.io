fc0a2b8479a2dbcdea2f30d914943ddeb8032e5a
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-02-13 03:28:03 +0000

    Update script.tsx

--- script.tsx
+++ script.tsx
@@ -1,16 +1,1 @@
-const audio = new Audio("Justin Bieber - The Kid LAROI - STAY.mp3");
-audio.loop = true;
-audio.volume = 1.0;
-audio.play();
-const Play = document.getElementById("Play");
-Play?.addEventListener("click", () => {
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
-});
-function render() {
-    return <button type="button" id="Play">Update Audio (Play/Pause)</button>
-}
-render();
+import three from 'three';
