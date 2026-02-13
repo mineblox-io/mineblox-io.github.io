cc147d733a96a41fb14a30b5eba99caebecbedf1
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-02-13 03:34:33 +0000

    Updated Scene

--- script.tsx
+++ script.tsx
@@ -13,3 +13,8 @@ audioLoader.load( 'Craig_Ballie_-_Building_Us_(mp3.pm).mp3', function( buffer ) 
 	sound.setVolume( 0.5 );
 	sound.play();
 });
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+const scene = new THREE.Scene();
