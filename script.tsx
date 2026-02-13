15c9698fc5b1ec3846c656dc840cc2b117747138
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-02-13 04:38:32 +0000

    Added React

--- script.tsx
+++ script.tsx
@@ -1,11 +1,10 @@
 import * as THREE from 'three';
+import * as react from 'react';
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 // create an AudioListener and add it to the camera
 const listener = new THREE.AudioListener();
 camera.add( listener );
-// create a global audio source
 const sound = new THREE.Audio( listener );
-// load a sound and set it as the Audio object's buffer
 const audioLoader = new THREE.AudioLoader();
 audioLoader.load( 'Craig_Ballie_-_Building_Us_(mp3.pm).mp3', function( buffer ) {
 	sound.setBuffer( buffer );
